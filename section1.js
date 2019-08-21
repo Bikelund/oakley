38ca14340b6c7c94d243e15badd58b2665afbfb6
Bikelund <nicklas_barklund@hotmail.com>
2019-08-21 14:54:53 +0200

    CSS-styling

--- section1.js
+++ section1.js
@@ -3,6 +3,9 @@ let textWrapper = document.querySelector('.intro-text1');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline().add({
+    targets: '.intro-text',
+    delay: 100
+}).add({
     targets: '.intro-text1 .letter',
     scale: [4, 1],
     opacity: [0, 1],
@@ -21,25 +24,4 @@ anime.timeline().add({
     delay: function(el, i) {
         return 400 *(i+1);
     }
-})
-
-
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: function(el, i) {
-      return 70*i;
-    }
-  }).add({
-    targets: '.ml2',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+});
